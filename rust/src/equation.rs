bd53e52220814e454c4369c8da453d506d5d85e4
simon0302010 <simon@hackclub.app>
2026-03-18 13:54:20 +0100

    Equation solving now from editor. Idk how to describe just checkout and test

--- rust/src/equation.rs
+++ rust/src/equation.rs
@@ -33,40 +33,55 @@ impl Equation {
 }
 
 pub fn solve_equation(data: &impl IntoEquation, mut input_man: &mut InputManager) -> String {
-    let mut res= "Error".to_string();
+    let mut res = "Error".to_string();
     if let Some(equation) = data.into_equation() {
+        let math = MathCore::new();
+
         let mut vars = equation.get_variables();
         // remove automatically set consts
         vars.remove("e");
         vars.remove("pi");
         vars.remove("tau");
-        if let Some(out) = select_var(&vars, &mut input_man) {
-            let math = MathCore::new();
+
+        let solve_res;
+        let prefix;
+
+        if vars.is_empty() {
+            solve_res = math.evaluate(&equation.data.to_string()).and_then(|r| Ok(vec![r]));
+            prefix = String::new();
+        } else {
+            let selres = select_var(&vars, &mut input_man);
+            if selres.is_none() { return "Failed to prompt user to select variable".to_string(); }
+            let selres = selres.unwrap();
+
             let engine = Engine::new();
-            vars.remove(&out);
+            vars.remove(&selres);
             let mut expr = equation.data;
             for var in &vars {
                 let res = input_number_for(var, &mut input_man, &math);
                 expr = engine.substitute(&expr, var, &res).unwrap_or(expr);
             }
 
-            match MathCore::solve(expr.to_string().as_str(), &out) {
-                Ok(r) => {
-                    if r.is_empty() {
-                        res = "No results".to_string();
-                    } else if r.len() == 1 {
-                        if let Expr::Number(n) = r[0] {
-                            res = format!("{} = {:.10}", out, n)
-                        } else {
-                            res = format!("{} = {}", out, r[0])
-                        }
+            solve_res = MathCore::solve(expr.to_string().as_str(), &selres);
+            prefix = format!("{} = ", selres);
+        }
+
+        match solve_res {
+            Ok(r) => {
+                if r.is_empty() {
+                    res = "No results".to_string();
+                } else if r.len() == 1 {
+                    if let Expr::Number(n) = r[0] {
+                        res = format!("{}{:.10}", prefix, n)
                     } else {
-                        res = format!("{} = {:?}", out, r);
+                        res = format!("{}{}", prefix, r[0])
                     }
-                },
-                Err(e) => {
-                    res = e.to_string();
+                } else {
+                    res = format!("{}{:?}", prefix, r);
                 }
+            },
+            Err(e) => {
+                res = e.to_string();
             }
         }
     }
